c968b7c805d7ea8902cb36de1849583477de6782
David Ingram <david@ingramaviation.com>
2019-10-13 20:34:14 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+    end
+
+    get '/' do
+        erb :index
+    end 
+
